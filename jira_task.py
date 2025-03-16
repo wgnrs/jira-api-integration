a2f17be44729bc97d833b6b04051541e6448bbd4
WagnerSJ-Atlas <wagner.silva@atlasinovacoes.com.br>
2025-03-16 20:21:14 -0300

    feat: metodo request de login

--- jira_task.py
+++ jira_task.py
@@ -2,7 +2,7 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 
-def get_task_info(issue_key, email, api_token):
+def make_request(issue_key, email, api_token):
     url = f"https://atlasinovacoes-ws-testes-consumacao.atlassian.net/rest/api/3/issue/{issue_key}"
     auth = HTTPBasicAuth(email, api_token)
     headers = {
@@ -10,9 +10,29 @@ def get_task_info(issue_key, email, api_token):
     }
 
     response = requests.get(url, headers=headers, auth=auth)
+    return response
+
+
+def get_task_info(issue_key, email, api_token):
+    response = make_request(issue_key, email, api_token)
     
     if response.status_code == 200:
         print(json.dumps(response.json(), indent=4, ensure_ascii=False))
         return response.json()
+    else:
+        return f"Erro {response.status_code}: {response.text}"
+
+def get_task_details(issue_key, email, api_token):
+    response = make_request(issue_key, email, api_token)
+
+    if response.status_code == 200:
+        task_data = response.json()        
+        details = {
+            "summary": task_data["fields"]["summary"],
+            "status": task_data["fields"]["status"]["name"],
+            "assignee": task_data["fields"]["assignee"]["displayName"] if task_data["fields"]["assignee"] else "Unassigned"
+        }
+
+        return details
     else:
         return f"Erro {response.status_code}: {response.text}"
